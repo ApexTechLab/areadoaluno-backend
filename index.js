302da29dca0eb8ad5ae3de35f08671386f17b828
Marcio Santos <fisico227@gmail.com>
2019-06-13 19:21:17 -0300

    Cria pasta de rotas e popula objeto classes.json

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 var app = express();
 
@@ -18,63 +17,9 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/api/student', (req, res) => {
-    fs.readFile('data/students.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).json({ status: 'error', result: err });
-        } else {
-            const obj = JSON.parse(data);
-            let isFound = false;
 
-            obj.students.forEach(student => {
-                if (student != null && student.id == req.query.id) {
-                    isFound = true;
-                    res.json({ status: 'success', result: student });
-                }
-            })
-            
-            if (!isFound) {
-                res.status(404).json({ status: 'not found', result: `Student with id ${req.query.id} not found` });
-            }
-        }
-    })
-});
+app.use('/api/student', require('./routes/student').router);
 
-app.post('/api/student', (req, res) => {
-    fs.readFile('data/students.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).json({ status: 'error', result: err });
-        } else {
-            const obj = JSON.parse(data);
-
-            req.body.id = obj.students.length + 1;
-
-            obj.students.push(req.body);
-
-            fs.writeFile('data/students.json', JSON.stringify(obj), function(err) {
-                if (err) {
-                    var response = { status: 'error', result: err };
-                    res.status(500).json(response);
-                } else {
-                    var response = { status: 'success', result: req.body };
-                    res.json(response);
-                }
-            });
-        }
-    })
-});
-
-app.get('/api/students', (req, res) => {
-    fs.readFile('data/students.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).json({ status: 'error', result: err });
-        } else {
-            const obj = JSON.parse(data);
-
-            res.json({ status: 'success', result: obj.students });
-        }
-    })
-});
 
 app.listen(9090, function() {
     console.log('Servidor rodando na porta 9090')
---
+++ routes/student.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+
+router.post('/', (req, res) => {
+    fs.readFile('data/students.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ status: 'error', result: err });
+        } else {
+            const obj = JSON.parse(data);
+
+            req.body.id = obj.students.length + 1;
+
+            obj.students.push(req.body);
+
+            fs.writeFile('data/students.json', JSON.stringify(obj), function(err) {
+                if (err) {
+                    var response = { status: 'error', result: err };
+                    res.status(500).json(response);
+                } else {
+                    var response = { status: 'success', result: req.body };
+                    res.json(response);
+                }
+            });
+        }
+    })
+});
+
+router.get('/', (req, res) => {
+    fs.readFile('data/students.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ status: 'error', result: err });
+        } else {
+            const obj = JSON.parse(data);
+
+            if(req.query.id) {
+                let isFound = false;
+
+                obj.students.forEach(student => {
+                    if (student != null && student.id == req.query.id) {
+                        isFound = true;
+                        res.json({ status: 'success', result: student });
+                    }
+                })
+                
+                if (!isFound) {
+                    res.status(404).json({ status: 'not found', result: `Student with id ${req.query.id} not found` });
+                }
+            } else {
+                res.json({ status: 'success', result: obj.students });
+            }
+        }
+    })
+});
+
+module.exports.router = router;
